20df8497f07afa998a03124e6175380e725a8ef4
Jan Lelis <hi@ruby.consulting>
2020-04-12 20:10:17 +0200

    Introduce palava papagei & github

---
+++ src/components/GithubRibbon.vue
@@ -0,0 +1,14 @@
+<template>
+  <a href="https://github.com/palavatv/palava">
+    <img src="@/assets/github.png" alt="Fork us on GitHub" class="github-ribbon" />
+  </a>
+</template>
+
+<style scoped>
+.github-ribbon {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <GithubRibbon />
+    <img alt="palava papagei" src="@/assets/palava.svg">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import GithubRibbon from '@/components/GithubRibbon.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    GithubRibbon,
   },
 };
 </script>

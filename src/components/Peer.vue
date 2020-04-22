72223f87b89350bc7f36f865ee4fe916bb029a27
Jan Lelis <hi@ruby.consulting>
2020-04-22 16:26:58 +0200

    Set proper css classes for each peer

--- src/components/Peer.vue
+++ src/components/Peer.vue
@@ -1,5 +1,18 @@
 <template>
-  <li>
+  <li :class="{
+    'peer': true,
+    'peer--local': peer.isLocal(),
+    'peer--remote': !peer.isLocal(),
+    'peer--is-ready': peer.isReady(),
+    'peer--not-ready': !peer.isReady(),
+    'peer--has-audio': !peer.hasAudio(),
+    'peer--has-no-audio': !peer.hasAudio(),
+    'peer--text-only': !peer.hasAudio() && !peer.hasVideo(),
+    'peer--muted-by-user': mute,
+    // 'peer--has-video': !peer.hasVideo(),
+    // 'peer--has-no-video': !peer.hasVideo(),
+    'peer--has-error': !!peer.error,
+  }">
     <Stream :peer="peer" />
   </li>
 </template>
@@ -11,6 +24,11 @@ export default {
   props: ["peer"],
   components: {
     Stream,
+  },
+  data() {
+    return {
+      mute: false,
+    }
   },
 }
 </script>

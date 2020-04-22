96aeaf37bd498aae53798e51b4ebaca50fe286e8
Jan Lelis <hi@ruby.consulting>
2020-04-22 18:01:55 +0200

    Also disable click key rule (space and enter key are working)

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'prefer-destructuring': 'off',
     'vue-a11y/media-has-caption': 'off',
     'vue-a11y/no-autofocus': 'off',
+    'vue-a11y/click-events-have-key-events': 'off',
   },
   overrides: [
     {

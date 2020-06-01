ec0af4903140ca07462b20d7b330eff93f9f7247
Jan Lelis <hi@ruby.consulting>
2020-06-01 10:56:33 +0200

    Add build option to disable minifaction

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  chainWebpack: (config) => config.resolve.symlinks(false) // see https://github.com/vuejs/vue-cli/issues/2948
+  chainWebpack: (config) => {
+    config.resolve.symlinks(false) // see https://github.com/vuejs/vue-cli/issues/2948
+    config.optimization.minimize(!process.env.BUILD_NOT_MINIFIED)
+  }
 }

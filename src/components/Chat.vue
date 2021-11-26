f54bcf56f3e1dbf354262f34bf4f6cbb993c20a8
Paul Götze <pgoetze@minddoc.de>
2021-11-26 21:01:21 +0100

    Beautify the chat

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -2,25 +2,32 @@
   <li class="peer chat-peer">
       <div class="chat">
         <div class="messages-area">
+          <div class="message-and-avatar message-and-avatar--right" v-if="peerCount == 1">
+            <span class="message message-error" v-html="$t('chat.error')"></span>
+          </div>
           <div class="message-and-avatar"
             v-for="chatMessage in chatMessages" :key="chatMessage.time"
             :class="{'message-and-avatar--right': chatMessage.from !== localPeerId}">
-            <PeerAvatar :peerId="chatMessage.from" v-if="chatMessage.from !== 'error'" />
-            <span class="message" :title="chatMessage.time" v-html="escapeHTML(chatMessage.msg)"
-                  :class="{'message-error': chatMessage.from === 'error'}">
+            <PeerAvatar :peerId="chatMessage.from" />
+            <span class="message" :title="chatMessage.time" v-html="escapeHTML(chatMessage.msg)">
             </span>
           </div>
         </div>
         <form class="send-area">
-          <input type="text" aria-label="Write message" v-model="newChatMessage" />
-          <button type="submit" @click.prevent="sendChatMessage()">▸</button>
+          <input type="textarea" aria-label="Write message" v-model="newChatMessage" />
+          <button type="submit" @click.prevent="sendChatMessage()">
+            <inline-svg
+              :alt="$t('chat.sendAlt')"
+              :src="require('../assets/send.svg')"
+              />
+          </button>
         </form>
       </div>
   </li>
 </template>
 
 <script>
-import PeerAvatar from "@/components/PeerAvatar.vue"
+import PeerAvatar from '@/components/PeerAvatar.vue'
 
 export default {
   props: {
@@ -81,7 +88,7 @@ export default {
 }
 
 .chat {
-  background: pink;
+  background: $chat-background;
   height: 100%;
   width: 100%;
   display: flex;
@@ -94,24 +101,31 @@ export default {
   flex: 1;
   display: flex;
   flex-direction: column-reverse;
-  margin-bottom: 1em;
+  margin-bottom: 0.5em;
 
   .message-and-avatar {
     display: flex;
-    align-items: center;
-    margin: 0.3em 1em;
+    align-items: flex-start;
+    margin: 0.3em 0.65em;
 
     .message {
-      display: inline-block;
+      display: inline-flex;
       margin: 0 0.2em;
-      padding: 0 0.5em;
+      padding: 0.25em 0.5em;
       background-color: white;
       border-radius: 3px;
+      text-align: left;
+      word-break: break-word;
     }
 
     .message-error {
       color: white;
-      background-color: red;
+      background-color: lighten($action-3, 15%);
+      padding: 0.5em 0.75em;
+    }
+
+    .avatar {
+      margin: 0.1em;
     }
 
     &--right {
@@ -122,11 +136,42 @@ export default {
 }
 
 .send-area {
-  width: 100%;
   display: flex;
   flex-direction: row;
+  align-items: center;
+  padding: 0.25em;
+
   input {
     flex: 1;
+    border-radius: 3px;
+    padding: 0.5em 0.5em;
+    padding-right: 2em;
+    background-color: white;
+    border: 0.1em solid $gray;
+
+    &:focus {
+      border: 0.1em solid darken($chat-background, 30%);
+
+      +button {
+        color: darken($chat-background, 45%);
+      }
+    }
+  }
+
+  button {
+    position: absolute;
+    cursor: pointer;
+    color: $chat-background;
+    fill: currentColor;
+    align-self: center;
+    width: $small-control-size;
+    height: $small-control-size;
+    padding: 0.25em;
+    right: 0.3em;
+
+    &:hover, &:focus {
+      color: $action-1;
+    }
   }
 }
 </style>
